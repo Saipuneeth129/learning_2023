6f80817e9a27129aa17e4c1ad927ab71523682f5
Saipuneeth129 <52091580+Saipuneeth129@users.noreply.github.com>
2023-06-17 07:45:17 +0530

    Update TEST1.c

--- Day3/Level 2/TEST1.c
+++ Day3/Level 2/TEST1.c
@@ -1,3 +1,14 @@
+/*Level-2
+  Day-3
+1. Bit Operations:
+https://1.bp.blogspot.com/-Tfj_D_f_AF0/VZO5QIIGAUI/AAAAAAAAAmI/5Rw6uIU3EiI/s1600/DS1307%2BTime%2BKeeper%2BRegister.PNG
+
+DS1307 has its time registers in BCD format as shown in the image found in the above link, so 
+a. Write a function to Set, clear and toggle bit of an 8-bit number
+
+Concepts to be used.
+- Operators
+*/
 #include <stdio.h>
 
 // Function to set a bit at a given position
