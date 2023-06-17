aa7e4673c631661ed1aa36d74d33b41263c1067c
Saipuneeth129 <52091580+Saipuneeth129@users.noreply.github.com>
2023-06-17 07:36:59 +0530

    Update TeSt1.c

--- Day2/Level 2/TeSt1.c
+++ Day2/Level 2/TeSt1.c
@@ -1,3 +1,15 @@
+/* Level-2
+   Day-2
+1. Write a program to swap every alternate element of a given array starting for element 0
+For example, let's say we have a[5] = {10, 20, 30, 40, 50}, the result should be a[5] = {30, 20, 10, 40, 50}
+one more example a[5] = {10, 20, 30, 40, 50, 60}, the result should be a[5] = {30, 20, 10, 60, 50, 40}
+
+Topics to be covered
+- Arrays
+- Loops
+- Basic Operators
+*/
+
 #include <stdio.h>
 
 void swapAlternateElements(int arr[], int size) {
