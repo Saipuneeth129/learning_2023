dc03405cffc81389207781cb5111792e1b299d8d
Saipuneeth129 <52091580+Saipuneeth129@users.noreply.github.com>
2023-06-17 07:16:00 +0530

    Update test2.c

--- Day1/Level 1/test2.c
+++ Day1/Level 1/test2.c
@@ -1,3 +1,10 @@
+/*2. Write a program to print the grade of the given student using if else if block
+    - 90 to 100 -> "Grade A"
+    - 75 to 89 -> "Grade B"
+    - 60 to 74 -> "Grade C"
+    - 50 to 59 -> "Grade D"
+    - 0 to 49 -> "Grade F" */
+
 #include <stdio.h>
 
 int main() {
