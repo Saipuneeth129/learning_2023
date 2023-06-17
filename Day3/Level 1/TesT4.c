73b60f83a8e7cb21487186d79e2d57461831e9ed
Saipuneeth129 <52091580+Saipuneeth129@users.noreply.github.com>
2023-06-17 07:44:29 +0530

    Update TesT4.c

--- Day3/Level 1/TesT4.c
+++ Day3/Level 1/TesT4.c
@@ -1,3 +1,8 @@
+/*Level-1
+  Day-3
+5. Write a program to find both smallest and largest digits in given n numbers
+For example let's say we have n = 3, and n1 = 8, n2 = 156, n3 = 123450, the result will be "Not Valid", 1 and 6, 0 and 5
+*/
 #include <stdio.h>
 
 // Function to find the smallest digit in a number
