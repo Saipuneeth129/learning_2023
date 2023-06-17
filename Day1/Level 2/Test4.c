586dc87b4f0bc813f3d3fa1f5381da5040222cdd
Saipuneeth129 <52091580+Saipuneeth129@users.noreply.github.com>
2023-06-17 07:30:09 +0530

    Update Test4.c

--- Day1/Level 2/Test4.c
+++ Day1/Level 2/Test4.c
@@ -1,3 +1,11 @@
+/* level-2 day-2
+    4. Write a function to find the type of the character passed
+    - 'A' to 'Z', return 1
+    - 'a; to 'z', return 2
+    - '0' to '9', return 3
+    - Any Other printable symbols, return 4
+    - Non printable symbols, return 5 */
+
 #include <stdio.h>
 #include <ctype.h>
 
