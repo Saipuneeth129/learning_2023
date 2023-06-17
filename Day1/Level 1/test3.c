c6828f4853a9a959b04ec4b126739ae6fd7d0417
Saipuneeth129 <52091580+Saipuneeth129@users.noreply.github.com>
2023-06-17 07:16:43 +0530

    Update test3.c

--- Day1/Level 1/test3.c
+++ Day1/Level 1/test3.c
@@ -1,3 +1,9 @@
+/*Write a program to read the 
+    - Roll No
+    - Name (Note, you should read full name of the student including initials)
+    - Marks of Physics, Math and Chemistry
+    Calculate the total, percentage and print the summary.*/
+
 #include <stdio.h>
 
 int main() {
