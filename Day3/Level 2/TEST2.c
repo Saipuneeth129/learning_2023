e0a78347a297745c0da4c5c059dd1e85023d829b
Saipuneeth129 <52091580+Saipuneeth129@users.noreply.github.com>
2023-06-17 07:45:56 +0530

    Update TEST2.c

--- Day3/Level 2/TEST2.c
+++ Day3/Level 2/TEST2.c
@@ -1,3 +1,14 @@
+/*Level-2
+  Day-3
+2. BCD to Decimal:
+https://1.bp.blogspot.com/-Tfj_D_f_AF0/VZO5QIIGAUI/AAAAAAAAAmI/5Rw6uIU3EiI/s1600/DS1307%2BTime%2BKeeper%2BRegister.PNG
+
+DS1307 has its time registers in BCD format as shown in the image found in the above link, To set the time we need to convert the decimal number to BCD format, hence, write a fucntion to convert decimal to BCD
+
+Concepts to be used.
+- Operators
+- Preprocessor
+*/
 #include <stdio.h>
 
 void decimalToBCD(int decimal, int *bcd)
